e4e69b6e05690d1c70017cf3d3a5c644e4be2e76
carlosvfb <cacavfb2107@gmail.com>
2025-01-20 14:16:04 -0300

    configurações do ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        plugins: {
+            "@stylistic/js": stylisticJs
+        },
+        rules: {
+            "@stylistic/js/indent": ["error", 4],
+            "@stylistic/js/quotes": ["error", "double"],
+            "@stylistic/js/semi": ["error", "always"]
+        }
+    },
+    {files: ["**/*.{js,mjs,cjs,ts}"]},
+    {languageOptions: { globals: globals.node }},
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+];
